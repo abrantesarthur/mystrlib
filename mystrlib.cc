e8649bd2ccd06afb0c532961d283ec4a3770a7b7
Arthur Abrantes <aabrantes@college.harvard.edu>
2020-05-02 09:57:10 -0400

    initial commit

---
+++ mystrlib.cc
@@ -0,0 +1,19 @@
+/*
+string integerToString(int n)
+
+int stringToInteger(string str)
+
+string floatToString(float f)
+
+string toUpperCase(string str)
+
+string toLowerCase(string str)
+
+bool equalsIgnoreCase(string str1, str2)
+
+bool startsWith(string str, string prefix)
+
+bool endsWith(string str, string suffix)
+
+void trim(str)
+*/
