3d36455129ee37a4b832cbab9162a8e8d9f33ed9
Arthur Abrantes <aabrantes@college.harvard.edu>
2020-05-02 10:19:46 -0400

    endsWith implemented

--- mystrlib.cc
+++ mystrlib.cc
@@ -13,7 +13,38 @@ bool equalsIgnoreCase(string str1, str2)
 
 bool startsWith(string str, string prefix)
 
-bool endsWith(string str, string suffix)
-
 void trim(str)
 */
+#include "mystrlib.hh"
+
+bool endsWith(string str, string suffix) {
+
+	int sfxlen = suffix.length();
+	int strlen = str.length();
+
+	if(sfxlen == 0) return true;
+
+	if(sfxlen > strlen) return false;
+
+	for(int i = 0; i < sfxlen; ++i) {
+		if(suffix[i] != str[strlen - sfxlen + i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main() {
+	string str = "arthur";
+	string suffix = "oarthur";
+
+	bool b = endsWith(str, suffix);
+
+	if(b) {
+		cout << "true" << endl;
+	} else {
+		cout << "false" << endl;
+	}
+}
+
---
+++ mystrlib.hh
@@ -0,0 +1,28 @@
+#ifndef MYSTRLIB_HH
+#define MYSTRLIB_HH
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// string integerToString(int n)
+
+// int stringToInteger(string str)
+
+// string floatToString(float f)
+
+// string toUpperCase(string str)
+
+// string toLowerCase(string str)
+
+// bool equalsIgnoreCase(string str1, str2)
+
+// bool startsWith(string str, string prefix)
+
+// bool endsWith(string str, string suffix)
+
+// void trim(str)
+
+bool endsWith(string str, string suffix);
+
+#endif
